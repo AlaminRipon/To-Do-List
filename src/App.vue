03fac7ec027ff66dc2b286026cbc69294281bcdd
alamin <alaminripon5654@gmail.com>
2022-03-24 12:29:49 +0600

    Adding an array or list of todo item

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,32 @@
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <to-do-item></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 
+
 <script>
 import ToDoItem from './components/ToDoItem.vue';
+import uniqueId from 'lodash.uniqueid';
 
 export default {
   name: 'app',
   components: {
     ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false},
+        { id: uniqueId('todo-'), label: 'Create a vue project with the CLI', done: true},
+        { id: uniqueId('todo-'), label: 'Have fan', done: true},
+        { id: uniqueId('todo-'), label: 'Create a to-do-list', done: false}
+      ]
+    };
   },
 };
 </script>
